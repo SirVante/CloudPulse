64a239e197719c77d208fc641269d7c184449a0f
DeVante Givens <givensdevante@yahoo.com>
2026-06-29 12:03:27 -0700

    Create install-cloudwatch-agent.sh

---
+++ scripts/install-cloudwatch-agent.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Updating system packages..."
+sudo dnf update -y 
+
+echo "Installing Amazon CloudWatch Agent..." 
+sudo dnf install amazon-cloudwatch-agent -y 
+
+echo "Enabling CloudWatch Agent..." 
+sudo systemctl enable amazon-cloudwatch-agent 
+
+echo"Starting CloudWatch Agent..." 
+sudo systemctl start amazon-cloudwatch-agent 
+
+echo "Checking CloudWatch Agent status..." 
+sudo systemctl status amazon-cloudwatch-agent --no-pager 
+
+echo "CloudWatch Agent installation complete."
